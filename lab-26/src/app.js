35af9d0467e298b984a39a94acf31a6dceba01a5
SajaSwalgah <saja.swalgah@yahoo.com>
2020-02-23 14:29:27 +0200

    multi files

--- lab-26/src/app.js
+++ lab-26/src/app.js
@@ -1,71 +1,9 @@
 import React from 'react';
-// import './app.scss';
+import Header from './header.js'
+import Counter from './counter.js'
+import Footer from './footer.js'
 
-
-const Header = ()=> {
-  return(
-    <header>
-      <h1>
-        Hello From Class 26
-      </h1>
-    </header>
-  )
-}
-
-const Footer = () => {
-  return(
-    <footer>
-      See U in Class 27
-    </footer>
-  )
-}
-
-class Counter extends React.Component{
-  constructor(props){
-    super(props);
-    this.state ={
-      counter : 0,
-    }
-  }
-
-  handleChange = e => {
-    let counter = e.target.value;
-    this.setState ({counter})
-  }
-
-  increaseClicks = c => {
-    c.preventDefault();
-    let counter = ++this.state.counter;
-    this.setState({counter})
-    console.log('inc counter',this.state);
-    
-  }
-
-  decreaseClicks = c => {
-    c.preventDefault();
-    let counter = --this.state.counter;
-    this.setState({counter})
-    console.log('dec counter',this.state);
-
-  }
-
-  render (){
-    return(
-      <section>
-        <h3>
-         counter = {this.state.counter}
-        </h3>
-        <button onClick={this.increaseClicks}>
-          click to increase counter
-        </button>
-        <button onClick={this.decreaseClicks}>
-          click to decrease counter
-        </button>
-      </section>
-    )
-  }
-}
-
+import './app.scss';
 
 class App extends React.Component {
   render() {
---
+++ lab-26/src/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Footer = () => {
+    return(
+      <footer>
+        See U in Class 27
+      </footer>
+    )
+  }
+
+  export default Footer;
